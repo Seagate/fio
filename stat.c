223d5a0fb8ce7c2727953ad7fc7badd8c1bc5466
Phillip Chen <phillip.a.chen@seagate.com>
2018-10-10 14:00:17 -0600

    Adding logging and documentation changes for new libaio prio_percent option.
    Signed-off-by: Phillip Chen <phillip.a.chen@seagate.com>

--- stat.c
+++ stat.c
@@ -472,9 +472,12 @@ static void show_ddir_status(struct group_run_stats *rs, struct thread_stat *ts,
 		display_lat("clat", min, max, mean, dev, out);
 	if (calc_lat(&ts->lat_stat[ddir], &min, &max, &mean, &dev))
 		display_lat(" lat", min, max, mean, dev, out);
+	if (calc_lat(&ts->clat_prio_stat, &min, &max, &mean, &dev) && ddir == DDIR_READ)
+		display_lat("PRIO_clat", min, max, mean, dev, out);
 
 	if (ts->clat_percentiles || ts->lat_percentiles) {
 		const char *name = ts->clat_percentiles ? "clat" : " lat";
+		char prio_name[32];
 		uint64_t samples;
 
 		if (ts->clat_percentiles)
@@ -486,6 +489,25 @@ static void show_ddir_status(struct group_run_stats *rs, struct thread_stat *ts,
 					samples,
 					ts->percentile_list,
 					ts->percentile_precision, name, out);
+
+        // Only print this if some priority stats were collected
+		if (ddir == DDIR_READ && ts->clat_prio_stat.samples > 0) {
+			sprintf(prio_name, "high prio (%.2f%%) %s",
+					100. * (double) ts->clat_prio_stat.samples / (double) samples,
+					name);
+			show_clat_percentiles(ts->io_u_plat_prio,
+						ts->clat_prio_stat.samples,
+						ts->percentile_list,
+						ts->percentile_precision, prio_name, out);
+
+			sprintf(prio_name, "low prio (%.2f%%) %s",
+					100. * (double) ts->clat_low_prio_stat.samples / (double) samples,
+					name);
+			show_clat_percentiles(ts->io_u_plat_low_prio,
+						ts->clat_low_prio_stat.samples,
+						ts->percentile_list,
+						ts->percentile_precision, prio_name, out);
+		}
 	}
 	if (calc_lat(&ts->bw_stat[ddir], &min, &max, &mean, &dev)) {
 		double p_of_agg = 100.0, fkb_base = (double)rs->kb_base;
@@ -1108,6 +1130,119 @@ static void add_ddir_status_json(struct thread_stat *ts,
 		}
 	}
 
+
+	// Only print PRIO latencies if some high priority samples were gathered
+	if (ts->clat_prio_stat.samples > 0) {
+		// START of PRIO CLAT
+
+	    if (ddir == DDIR_READ) {
+		    if (!calc_lat(&ts->clat_prio_stat, &min, &max, &mean, &dev)) {
+				min = max = 0;
+				mean = dev = 0.0;
+			}
+			tmp_object = json_create_object();
+			json_object_add_value_object(dir_object, "clat_prio",
+					tmp_object);
+			json_object_add_value_int(tmp_object, "samples",
+					ts->clat_prio_stat.samples);
+			json_object_add_value_int(tmp_object, "min", min);
+			json_object_add_value_int(tmp_object, "max", max);
+			json_object_add_value_float(tmp_object, "mean", mean);
+			json_object_add_value_float(tmp_object, "stddev", dev);
+
+			if (ts->clat_percentiles) {
+				len = calc_clat_percentiles(ts->io_u_plat_prio,
+							ts->clat_prio_stat.samples,
+							ts->percentile_list, &ovals, &maxv,
+							&minv);
+			} else
+				len = 0;
+
+			percentile_object = json_create_object();
+			json_object_add_value_object(tmp_object, "percentile", percentile_object);
+			for (i = 0; i < FIO_IO_U_LIST_MAX_LEN; i++) {
+				if (i >= len) {
+					json_object_add_value_int(percentile_object, "0.00", 0);
+					continue;
+				}
+				snprintf(buf, sizeof(buf), "%f", ts->percentile_list[i].u.f);
+				json_object_add_value_int(percentile_object, (const char *)buf, ovals[i]);
+			}
+
+			if (output_format & FIO_OUTPUT_JSON_PLUS) {
+				clat_bins_object = json_create_object();
+				json_object_add_value_object(tmp_object, "bins", clat_bins_object);
+				for(i = 0; i < FIO_IO_U_PLAT_NR; i++) {
+					snprintf(buf, sizeof(buf), "%d", i);
+					json_object_add_value_int(clat_bins_object, (const char *)buf,
+							ts->io_u_plat_prio[i]);
+				}
+				json_object_add_value_int(clat_bins_object, "FIO_IO_U_PLAT_BITS",
+						FIO_IO_U_PLAT_BITS);
+				json_object_add_value_int(clat_bins_object, "FIO_IO_U_PLAT_VAL",
+						FIO_IO_U_PLAT_VAL);
+				json_object_add_value_int(clat_bins_object, "FIO_IO_U_PLAT_NR",
+						FIO_IO_U_PLAT_NR);
+			}
+	    }
+
+		// END OF PRIO CLAT
+
+		// START of LOW PRIO CLAT
+
+	    if (ddir == DDIR_READ) {
+		    if (!calc_lat(&ts->clat_low_prio_stat, &min, &max, &mean, &dev)) {
+				min = max = 0;
+				mean = dev = 0.0;
+			}
+			tmp_object = json_create_object();
+			json_object_add_value_object(dir_object, "clat_low_prio",
+					tmp_object);
+			json_object_add_value_int(tmp_object, "samples",
+					ts->clat_low_prio_stat.samples);
+			json_object_add_value_int(tmp_object, "min", min);
+			json_object_add_value_int(tmp_object, "max", max);
+			json_object_add_value_float(tmp_object, "mean", mean);
+			json_object_add_value_float(tmp_object, "stddev", dev);
+
+			if (ts->clat_percentiles) {
+				len = calc_clat_percentiles(ts->io_u_plat_low_prio,
+							ts->clat_low_prio_stat.samples,
+							ts->percentile_list, &ovals, &maxv,
+							&minv);
+			} else
+				len = 0;
+
+			percentile_object = json_create_object();
+			json_object_add_value_object(tmp_object, "percentile", percentile_object);
+			for (i = 0; i < FIO_IO_U_LIST_MAX_LEN; i++) {
+				if (i >= len) {
+					json_object_add_value_int(percentile_object, "0.00", 0);
+					continue;
+				}
+				snprintf(buf, sizeof(buf), "%f", ts->percentile_list[i].u.f);
+				json_object_add_value_int(percentile_object, (const char *)buf, ovals[i]);
+			}
+
+			if (output_format & FIO_OUTPUT_JSON_PLUS) {
+				clat_bins_object = json_create_object();
+				json_object_add_value_object(tmp_object, "bins", clat_bins_object);
+				for(i = 0; i < FIO_IO_U_PLAT_NR; i++) {
+					snprintf(buf, sizeof(buf), "%d", i);
+					json_object_add_value_int(clat_bins_object, (const char *)buf,
+							ts->io_u_plat_low_prio[i]);
+				}
+				json_object_add_value_int(clat_bins_object, "FIO_IO_U_PLAT_BITS",
+						FIO_IO_U_PLAT_BITS);
+				json_object_add_value_int(clat_bins_object, "FIO_IO_U_PLAT_VAL",
+						FIO_IO_U_PLAT_VAL);
+				json_object_add_value_int(clat_bins_object, "FIO_IO_U_PLAT_NR",
+						FIO_IO_U_PLAT_NR);
+			}
+	    }
+
+		// END OF LOW PRIO CLAT
+	}
 
 	if (!ddir_rw(ddir))
 		return;
@@ -1851,7 +1986,6 @@ void __show_run_stats(void)
 			ts->block_infos[k] = td->ts.block_infos[k];
 
 		sum_thread_stats(ts, &td->ts, idx == 1);
-		ts->nr_zone_resets += td->ts.nr_zone_resets;
 
 		if (td->o.ss_dur) {
 			ts->ss_state = td->ss.state;
@@ -1879,7 +2013,6 @@ void __show_run_stats(void)
 		rs->unit_base = ts->unit_base;
 		rs->sig_figs = ts->sig_figs;
 		rs->unified_rw_rep += ts->unified_rw_rep;
-		rs->nr_zone_resets += ts->nr_zone_resets;
 
 		for (j = 0; j < DDIR_RWDIR_CNT; j++) {
 			if (!ts->runtime[j])
